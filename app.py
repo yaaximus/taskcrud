9556ae6cabb1164f38fa314db8299ab971798484
Yasiim Ahmad <yasim.ahmed63@yahoo.com>
2026-04-28 17:27:30 +1000

    code refactored

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,6 +16,7 @@ class Todo(db.Model):
 
     def __repr__(self):
         return '<Task %r>' % self.id
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -38,6 +41,7 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
     
+
 @app.route('/delete/<int:id>')
 def delete(id):
     task_to_delete = Todo.query.get_or_404(id)
@@ -48,6 +52,7 @@ def delete(id):
         return redirect('/')
     except:
         return "There was a problem deleting that task"
+
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
@@ -63,6 +68,7 @@ def update(id):
             return "There was an issue updating your task"
     else:
         return render_template('update.html', task=task)
+
 
 if __name__ == "__main__":
 
